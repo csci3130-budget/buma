d45869b1aa7876d294fd63e9e9c0a8682018b913
wolkerw <contato@wolker.com.br>
2014-01-24 18:20:49 -0400

    changed files format

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Ruipeng">
+        <meta name="description" content="The website for Assignment1">
+        <title>BUMA</title>
+ 		<link href="css/style.css" rel="stylesheet" type="text/css" media="screen" >
+		<link href="css/format.css" rel="stylesheet" type="text/css" media="all" />
+ 	    <script type="text/javascript" src="js/scripts.js">
+        </script>
+    </head>
+
+    <body>
+    	<div id="wrap">
+            <div id="header">
+                <h1>BUMA</h1>
+            </div><?php
+            
+            include "/login.php";
+            
+            ?><div id="footer">    
+                <h2>Author:</h2>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ login.php
@@ -0,0 +1,7 @@
+<form action="login.php" id="login_form">
+    <p>Login</p>
+    <p><label>Username:</label> <input type="text" name="username" id="username" /></p>
+    <p><label>Password:</label> <input type="password" name="password" id="password" /></p>
+    <p><input type="submit" name="submit_login" value="Login" />  <a href="pwdreset.html" class="forget">Forgot password?</a></p>
+    <p> If you are not a member, please <a href="account/signup.php">Sign up</a></p>
+</form>
