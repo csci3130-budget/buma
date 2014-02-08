d5171b4948566fe983e76195e5f1a8917006bfac
wolkerw <contato@wolker.com.br>
2014-02-08 15:18:59 -0400

    Statistics page

--- index.php
+++ index.php
@@ -9,12 +9,13 @@ include_once dirname(__FILE__) . '/config.php';
         <meta name="description" content="The website for Assignment1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, , user-scalable=no">
         <title>BUMA</title>
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+ 	    <script type="text/javascript" src="js/event.js"></script>
         <link href="css/bootstrap.min.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet" type="text/css" media="screen" >
         <link href="css/format.css" rel="stylesheet" type="text/css" media="all" />
- 	    <script type="text/javascript" src="js/jquery-1.11.0.min.js"></script>
- 	    <!--<script type="text/javascript" src="js/scripts.js"></script>-->
- 	    <script type="text/javascript" src="js/event.js"></script>
+        <script src="https://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </head>
 
     <body>
@@ -35,7 +36,6 @@ include_once dirname(__FILE__) . '/config.php';
                     <li><a href="add_expense"><span class="glyphicon glyphicon-usd"></span>&nbsp;&nbsp;Add New Expense</a></li> <!--#Add-->
                     <li><a href="wish_list"><span class="glyphicon glyphicon-gift"></span>&nbsp;&nbsp;Wish List</a></li> <!--#Wish-->
                     <li><a href="statistics"><span class="glyphicon glyphicon-stats"></span>&nbsp;&nbsp;Statistics</a></li> <!--#Stats-->
-
                   </ul>
                 </div>
             </div>
@@ -43,13 +43,8 @@ include_once dirname(__FILE__) . '/config.php';
         <div class="container"><?php
         
      	// Show current page
-		echo include dirname(__FILE__);
-		echo $file;
 	    include dirname(__FILE__) . '/' . $file . '.php';
-		//http://web.cs.dal.ca/~wegner/
         
         ?></div>
-        <script src="https://code.jquery.com/jquery.js"></script>
-        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
