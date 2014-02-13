72549f95c6674d2207a5fae4dbee0968a0eb7d81
Sarah Morash <morash@bluenose.cs.dal.ca>
2014-02-12 23:55:08 -0400

    Login links

---
+++ forgot_password.php
@@ -0,0 +1,8 @@
+<!-- When form is filled out, email user when their password information -->
+<form class="form-signin" role="form">
+    <h2 class="form-signin-heading">Forgot Password</h2>
+    <input type="text" class="form-control" placeholder="Email address" required autofocus>
+    <button type="button" class="btn btn-link forgot-control">Forgot Password</button>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Send</button>
+</form>
+
--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <form class="form-signin" role="form">
-    <h2 class="form-signin-heading">Welcome to BUMA</h2>
-    <input type="text" class="form-control" placeholder="Email address" required autofocus>
-    <input type="password" class="form-control" placeholder="Password" required>
+    <h2 class="form-signin-heading">welcome to buma</h2>
+    <input type="text" class="form-control" placeholder="email address" required autofocus>
+    <input type="password" class="form-control" placeholder="password" required>
     <label class="checkbox checkbox-control">
-      <input type="checkbox" value="remember-me"> Remember me
+      <input type="checkbox" value="remember-me"> remember me
     </label>
-    <button type="button" class="btn btn-link forgot-control">Forgot Password</button>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
-	<button class="btn btn-default btm-new-account" type="button">Create New Account</button>
+    <a href="forgot_password"><button type="button" class="btn btn-link forgot-control">forgot password</button></a>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">log in</button>
+	<a href="create_account"><button class="btn btn-default btm-new-account" type="button">create new account</button></a>
 </form>
 
