0dd0399d95408b681d2b2ba7724b242f583633ab
Sarah Morash <sarah.morash@dal.ca>
2014-03-04 18:44:40 -0400

    Added a wack load of tests - we had many issues getting codeception to run tests, but our TA Hui told us to just keep going with creating the tests, so were now commiting tests from the past few weeks - our documentation will inform you of the exact days these tests were completed

--- tests/acceptance/AddBudgetCept.php
+++ tests/acceptance/AddBudgetCept.php
@@ -2,7 +2,7 @@
 // AddBudget
 $I = new WebGuy($scenario);
 $I->wantTo('Add a new budget');
-$I->amOnPage('/index.php?file=wish_list');
+$I->amOnPage('/index.php?file=add_budget');
 
 // Looks for Amount and Category labels
 $I->see('Amount');
