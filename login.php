7f3e29aad7c32dba216068e2c714644a68b3fefe
zack-liruipeng <zack.liruipeng@gmail.com>
2014-03-06 18:50:51 -0400

    add to make ajax work

--- login.php
+++ login.php
@@ -14,50 +14,22 @@
 
     <body>
           	
-            <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-                <div class="container">
-                    <div class="navbar-header">
-                      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-                      <a class="navbar-brand" href="#">BUBA</a>
-                    </div>
-                    <div class="collapse navbar-collapse">
-                  <ul class="nav navbar-nav">
-                    <li><a href="index.php?file=add_budget"><span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;Add New Budget</a></li> <!--#Add-->
-                    <li><a href="index.php?file=add_expense"><span class="glyphicon glyphicon-usd"></span>&nbsp;&nbsp;Add New Expense</a></li> <!--#Add-->
-                    <li><a href="index.php?file=wish_list"><span class="glyphicon glyphicon-gift"></span>&nbsp;&nbsp;Wish List</a></li> <!--#Wish-->
-                    <li><a href="index.php?file=statistics"><span class="glyphicon glyphicon-stats"></span>&nbsp;&nbsp;Statistics</a></li> <!--#Stats-->
-				  </ul>
-                </div><!--/.nav-collapse -->
-                </div>
-            </div>
-            <div class="container">
-			
-		<!--login box-->	
-	<div id="login" class="visible widget-box no-border">
-		<form class="form-signin" role="form">
-        <h2 class="form-signin-heading">Welcome to BUBA</h2>
-        <input type="text" class="form-control" placeholder="Email address" required autofocus>
-        <input type="password" class="form-control" placeholder="Password" required>
-        <label class="checkbox checkbox-control">
-          <input type="checkbox" value="remember-me"> Remember me
-        </label>
-		
-       <a href="forgot">
-	   <button type="button" class="btn btn-link forgot-control">Forgot Password</button>
-	   </a>
-	   
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
-		
-		
-		<a href="register">
-		<button class="btn btn-default btm-new-account" type="button">Create New Account</button></a>
-      </form>	
-	</div
+    <form class="form-signin" role="form" id="login_form">
+    <h2 class="form-signin-heading">Welcome to BUMA</h2>
+    <input type="text" class="form-control" name="email" placeholder="Email address" required autofocus>
+    <input type="password" class="form-control" name="password" placeholder="Password" required>
+    <label class="checkbox checkbox-control">
+      <input type="checkbox" value="remember-me"> Remember me
+    </label>
+	<a href="forgot">
+    <button type="button" class="btn btn-link forgot-control" id="forgot_form" >Forgot Password</button>
+    </a>
+	<button class="btn btn-lg btn-primary btn-block" id="btn_login_form" type="button">Log in</button>
+	<a href="register">
+	<button class="btn btn-default btm-new-account" id="register_form" type="button">Create New Account</button>
+    </a>
+	<div class="alert"></div>
+</form>
 	
 	
 		</div>
