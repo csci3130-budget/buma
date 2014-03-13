4657c50e31e4831e5698a96630f996c170a2b945
dalter <dalter@centi.cs.dal.ca>
2014-03-12 21:23:08 -0300

    Deleted an echo test

--- remember_post.php
+++ remember_post.php
@@ -3,8 +3,5 @@
 include_once dirname(__FILE__) . '/../config.php';
 include_once dirname(__FILE__) . '/classes/login.php';
     setcookie('email', $_POST['email'], time()+ 31536000);
-    echo($_COOKIE['email']);
     setcookie('password', $_POST['password'], time()+ 31536000);
-    echo($_COOKIE['password']);
-
 ?>
