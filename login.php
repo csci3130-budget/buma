6fe834f5d7bb9c02427d12420d89c14b76a8d25f
Sarah Morash <sarah.morash@dal.ca>
2014-03-27 18:19:54 -0300

    Changed our app name to the right name :P

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
-<div id="login" class="visible widget-box no-border">
+<div id="login" id="loginForm" class="visible widget-box no-border">
 	<form class="form-signin" role="form"  id="login_form">
-        <h2 class="form-signin-heading">Welcome to BUBA</h2>
-        <input name="email" type="text" class="form-control" placeholder="Email address" required autofocus value="<?php
+        <h2 class="form-signin-heading">Welcome to BUMA</h2>
+        <input name="email" type="text" class="username form-control" placeholder="Email address" required autofocus value="<?php
 		echo $_COOKIE['email']; ?>">
-        <input name="password" type="password" class="form-control" placeholder="Password" required value="<?php
+        <input name="password" type="password" class="password form-control" placeholder="Password" required value="<?php
 		echo $_COOKIE['password']; ?>">
         <label class="checkbox checkbox-control"><input name="remember_me" type="checkbox" value="remember-me" <?php echo (isset($_COOKIE['email']) && isset($_COOKIE['password']) ? "checked='checked'" : ""); ?>> Remember me</label>
     
@@ -16,21 +16,3 @@
 	    <div id="login_form" class="alert"></div>
 	</form>	
 </div>
-
-<!--<script type="text/javascript">
-function show_box(id) {
-	$('.widget-box.visible').removeClass('visible');
-	$('#'+id).addClass('visible');
-}
-</script>
-		
-	
-<script type="text/javascript">
-var vglnk = vglnk || 
-{
-	api_url: '//api.viglink.com/api', 
-	key: '0dff9ade2d1125af6c910069b6d6e155', reaffiliate: false
-};
-</script>
-
-<script type="text/javascript" async="" src="./BUMA_files/vglnk.js"></script>-->
